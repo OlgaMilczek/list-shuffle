88962134f89120e0bbccad7cb98abdbeefba5f5a
Olga Milczek <olga.milczek@movecloser.pl>
2021-11-30 22:27:14 +0100

    Add Item component to molecules

---
+++ src/components/molecules/Item.vue
@@ -0,0 +1,30 @@
+<template>
+  <Box :class="{ 'border-primary': isActive }" :withShadow="isActive" >
+    <div class="p-3">
+      <template v-for="(value, name) in item">
+        <span :key="name">{{ name }}: <b>{{ value }}</b></span>
+      </template>
+    </div>
+  </Box>
+</template>
+
+<script lang="ts">
+
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+import { Box } from '@/components/atoms'
+
+@Component({
+  name: 'Item',
+  components: { Box }
+})
+export class Item extends Vue {
+  @Prop({ type: Object, required: true })
+  public readonly item!: Record<string, unknown>
+
+  @Prop({ type: Boolean, required: false, default: false })
+  public readonly isActive!: boolean
+}
+
+export default Item
+</script>
